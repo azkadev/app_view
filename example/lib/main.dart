99b37b11a07844d41818dcda74e188f37987b018
Your Name <you@example.com>
2022-08-24 16:40:03 +0700

    support mix json and widget

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,27 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:app_view/app_view.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(const MyHomePage());
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
+  const MyHomePage({Key? key}) : super(key: key);
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -38,20 +22,32 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: JsonToWidget({
-        "@type": "Column",
-        "children": [
-          {
-            "@type": "Padding",
-            "padding": {
-              "@type": "EdgeInsets.all",
-              "value": _counter,
+    return jsonToWidget({
+      "@type": "MaterialApp",
+      "debugShowCheckedModeBanner": true,
+      "home": {
+        "@type": "Scaffold",
+        "body": {
+          "@type": "Column",
+          "children": [
+            {
+              "@type": "Padding",
+              "padding": {
+                "@type": "EdgeInsets.all",
+                "value": _counter,
+              },
+              "child": {
+                "@type": "Center",
+                "child": {
+                  "@type": "Container",
+                  "child": {
+                    "@type": "Text",
+                    "data": "Hello world $_counter",
+                  }
+                }
+              },
             },
-            "child": {
+            {
               "@type": "Center",
               "child": {
                 "@type": "Container",
@@ -61,54 +57,49 @@ class _MyHomePageState extends State<MyHomePage> {
                 }
               }
             },
-          },
-          {
-            "@type": "Center",
-            "child": {
-              "@type": "Container",
-              "child": {
-                "@type": "Text",
-                "data": "Hello world $_counter",
-              }
-            }
-          },
-          {
-            "@type": "Padding",
-            "padding": const {
-              "@type": "EdgeInsets.symmetric",
-              "vertical": 50,
-            },
-            "child": {
+            {
               "@type": "Padding",
               "padding": const {
-                "@type": "EdgeInsets.all",
-                "value": 10,
+                "@type": "EdgeInsets.symmetric",
+                "vertical": 50,
               },
               "child": {
-                "@type": "Row",
-                "children": List.generate(5, (index) {
-                  return {
-                    "@type": "Padding",
-                    "padding": {
-                      "@type": "EdgeInsets.all",
-                      "value": 10,
-                    },
-                    "child": {
-                      "@type": "Text",
-                      "data": "Row Text $index",
-                    }
-                  };
-                })
+                "@type": "Padding",
+                "padding": const {
+                  "@type": "EdgeInsets.all",
+                  "value": 10,
+                },
+                "child": {
+                  "@type": "Row",
+                  "children": List.generate(5, (index) {
+                    return {
+                      "@type": "Padding",
+                      "padding": {
+                        "@type": "EdgeInsets.all",
+                        "value": 10,
+                      },
+                      "child": {
+                        "@type": "Text",
+                        "data": "Row Text $index",
+                      }
+                    };
+                  })
+                }
               }
-            }
-          },
-        ]
-      }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+            },
+          ]
+        },
+        "floatingActionButton": {
+          "@type": "floatingActionButton",
+          "onPressed": _incrementCounter,
+          "tooltip": 'Increment',
+          "child": Icon(Icons.abc),
+        }
+        // "floatingActionButton": FloatingActionButton(
+        //   onPressed: () {},
+        //   child: Icon(Icons.abc),
+        // )
+      }
+    });
   }
 }
--- lib/app_view.dart
+++ lib/app_view.dart
@@ -17,63 +17,76 @@ class JsonToWidget extends StatelessWidget {
 }
 
 /// json to widget function
-Widget jsonToWidget(Map? data) {
-  data ??= {};
-  if (data["@type"] is String) {
-    String type = data["@type"];
-    if (RegExp(r"^(helloWorld)$", caseSensitive: false).hasMatch(type)) {}
+Widget jsonToWidget(dynamic data) {
+  if (data is Widget) {
+    return data;
+  }
+  if (data is Map) {
+    if (data["@type"] is String) {
+      String type = data["@type"];
+      if (RegExp(r"^(helloWorld)$", caseSensitive: false).hasMatch(type)) {}
 
-    if (RegExp(r"^(MaterialApp)$", caseSensitive: false).hasMatch(type)) {
-      return MaterialApp(
-        home: jsonToWidget(data["home"]),
-      );
-    }
-    if (RegExp(r"^(Scaffold)$", caseSensitive: false).hasMatch(type)) {
-      return Scaffold(
-        body: jsonToWidget(data["body"]),
-      );
-    }
-    if (RegExp(r"^(Container)$", caseSensitive: false).hasMatch(type)) {
-      return Container(
-        child: jsonToWidget(data["child"]),
-      );
-    }
-    if (RegExp(r"^(Center)$", caseSensitive: false).hasMatch(type)) {
-      return Center(
-        child: jsonToWidget(data["child"]),
-      );
-    }
-
-    if (RegExp(r"^(Padding)$", caseSensitive: false).hasMatch(type)) {
-      return Padding(
-        padding: jsonToEdgeInsetsGeometry(data["padding"]),
-        child: jsonToWidget(data["child"]),
-      );
-    }
-
-    if (RegExp(r"^(Text)$", caseSensitive: false).hasMatch(type)) {
-      return Text("${data["data"]}");
-    }
-
-    if (RegExp(r"^(Column|Row)$", caseSensitive: false).hasMatch(type)) {
-      late List<Widget> children = [];
-      if (data["children"] is List<Map>) {
-        for (var i = 0; i < (data["children"] as List<Map>).length; i++) {
-          // ignore: non_constant_identifier_names
-          var loop_data = (data["children"] as List<Map>)[i];
-          try {
-            children.add(jsonToWidget(loop_data));
-          } catch (e) {}
-        }
+      if (RegExp(r"^(MaterialApp)$", caseSensitive: false).hasMatch(type)) {
+        return MaterialApp(
+          home: jsonToWidget(data["home"]),
+          debugShowCheckedModeBanner: (data["debugShowCheckedModeBanner"] == false) ? false : true,
+        );
       }
-      if (RegExp(r"^(Row)$", caseSensitive: false).hasMatch(type)) {
-        return Row(
+      if (RegExp(r"^(Scaffold)$", caseSensitive: false).hasMatch(type)) {
+        return Scaffold(
+          body: jsonToWidget(data["body"]),
+          floatingActionButton: jsonToWidget(data["floatingActionButton"]),
+        );
+      }
+      if (RegExp(r"^(Container)$", caseSensitive: false).hasMatch(type)) {
+        return Container(
+          child: jsonToWidget(data["child"]),
+        );
+      }
+      if (RegExp(r"^(FloatingActionButton)$", caseSensitive: false).hasMatch(type)) {
+        return FloatingActionButton(
+          onPressed: data["onPressed"],
+          tooltip: data["tooltip"],
+          child: jsonToWidget(data["child"]),
+        );
+      }
+      if (RegExp(r"^(Center)$", caseSensitive: false).hasMatch(type)) {
+        return Center(
+          child: jsonToWidget(data["child"]),
+        );
+      }
+
+      if (RegExp(r"^(Padding)$", caseSensitive: false).hasMatch(type)) {
+        return Padding(
+          padding: jsonToEdgeInsetsGeometry(data["padding"]),
+          child: jsonToWidget(data["child"]),
+        );
+      }
+
+      if (RegExp(r"^(Text)$", caseSensitive: false).hasMatch(type)) {
+        return Text("${data["data"]}");
+      }
+
+      if (RegExp(r"^(Column|Row)$", caseSensitive: false).hasMatch(type)) {
+        late List<Widget> children = [];
+        if (data["children"] is List<Map>) {
+          for (var i = 0; i < (data["children"] as List<Map>).length; i++) {
+            // ignore: non_constant_identifier_names
+            var loop_data = (data["children"] as List<Map>)[i];
+            try {
+              children.add(jsonToWidget(loop_data));
+            } catch (e) {}
+          }
+        }
+        if (RegExp(r"^(Row)$", caseSensitive: false).hasMatch(type)) {
+          return Row(
+            children: children,
+          );
+        }
+        return Column(
           children: children,
         );
       }
-      return Column(
-        children: children,
-      );
     }
   }
   return Container();
